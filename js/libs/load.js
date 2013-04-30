1068b16c08f3042dee7b23f46641b7a40151c355
Karthick Ramachandran <karthick.ramachandran@gmail.com>
2013-04-30 10:17:56 -0400

    disabling worker console

--- js/libs/load.js
+++ js/libs/load.js
@@ -3,7 +3,7 @@ define([
     'libs/require_plugins/order!libs/ember',
     'libs/require_plugins/order!libs/ember-data',
     'libs/require_plugins/order!libs/paper',
-    'libs/require_plugins/order!libs/WorkerConsole',
+    // 'libs/require_plugins/order!libs/WorkerConsole',
     'libs/require_plugins/order!libs/handlebars',
     'libs/require_plugins/order!libs/jquery-ui',
     'libs/require_plugins/order!libs/jquery-numeric',
